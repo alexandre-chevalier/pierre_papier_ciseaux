52f1bd4b2b415d777e8594758db9591dcf7ec3a9
alexandre <alexandre.chevalier@laplateforme.io>
2025-01-06 09:02:21 +0100

    2nd version du pierre papier ciseaux avec 2nd joueur qui choisi automatiquement un des 3 éléments

--- ppc.py
+++ ppc.py
@@ -1,34 +1,79 @@
 import random
 
-symbol = ["pierre","papier","ciseaux"];
-print(symbol);
+def game():
 
-playerName = input("veuillez entrez votre nom :")
-print("veuillez choisir un symbole")
+    symbol = ["pierre","papier","ciseaux"];
 
-for i in range(len(symbol)):
-    print(f"symbole {i} : " + symbol[i]);
+    print(f"veuillez choisir un symbole")
+
+    for i in range(len(symbol)):
+        print(f"symbole {i} : " + symbol[i]);
+
+    player = symbol[int(input("veuillez choisir un chiffre entre 0 et 2 pour choisir votre symbole : "))]
+    bot = symbol[random.randint(0,2)]
+
+    print(f"vous avez choisie : {player}")
+    print(f"le bot a choisie : {bot}")
+    listsymb = [player, bot];
+    print(listsymb)
+    return victory(listsymb);
+
+def victory(gameplay):
+    print("supersonic")
+    victoryGamePlayer = 0;
+    victoryGameBot = 0;
+
+    if victoryGameBot == 3:
+        return print("le bot a gagner")
+        replay();
+    elif victoryGamePlayer == 3:
+        return print(f"vous avez gagner {playerName}")
+        replay(playerName);
+    else:
+            if gameplay[0] == gameplay[1]:
+                print("ceci est une manche nulle")
+                game();
+            elif gameplay[0] == "ciseaux" and gameplay[1] == "papier":
+                print(f"vous avez gagner car le {gameplay[0]} bat le {gameplay[1]}")
+                victoryGamePlayer +=1;
+                game();
+            elif gameplay[0] == "papier" and gameplay[1] == "ciseaux":
+                print(f"je t'ai battu car le {gameplay[1]} bat le {gameplay[0]}")
+                victoryGameBot +=1
+                game();
+            elif gameplay[0] == "pierre" and gameplay[1] == "ciseaux":
+                print(f" vous avez gagner car le {gameplay[0]} bat le {gameplay[1]}")
+                victoryGamePlayer +=1;
+                game();
+            elif gameplay[0] == "ciseaux" and gameplay[1] == "pierre":
+                print(f"je t'ai battu car le {gameplay[1]} bat le {gameplay[0]}")
+                victoryGameBot +=1
+                game();
+            elif gameplay[0] == "papier" and gameplay[1] == "pierre":
+                print(f"vous avez gagner car le {gameplay[0]} bat le {gameplay[1]}")
+                victoryGamePlayer +=1;
+                game();
+            elif gameplay[0] == "pierre" and gameplay[1] == "papier":
+                print(f"je t'ai battu car le {gameplay[1]} bat le {gameplay[0]}")
+                victoryGameBot +=1
+                game();
+        
+       
+def replay(playername):
+    choice = input("Voulez vous rejouer ? (y/n)").upper();
+    if choice == "Y":
+        game();
+        victory(playerName, game(playerName));
+    else:
+        return print(f"merci d'avoir jouer {playername} ;)")
+    
 
 
-player1 = symbol[int(input("veuillez choisir un chiffre entre 0 et 2 pour choisir votre symbole : "))]
-player2 = symbol[random.randint(0,2)]
-
-print(f"{playerName}, vous avez choisie : {player1}")
-print(f"le bot a choisie : {player2}")
-
-if player1 == player2: 
-    print("ceci est une manche nulle")
-elif player1 == "ciseaux" and player2 == "papier":
-    print(f"{playerName} vous avez gagner car le {player1} bat le {player2}")
-elif player1 == "papier" and player2 == "ciseaux":
-    print(f"je t'ai battu {playerName}car le {player2} bat le {player1}")
-elif player1 == "pierre" and player2 == "ciseaux":
-    print(f"{playerName} vous avez gagner car le {player1} bat le {player2}")
-elif player1 == "ciseaux" and player2 == "pierre":
-    print(f"je t'ai battu {playerName}car le {player2} bat le {player1}")
-elif player1 == "papier" and player2 == "pierre":
-    print(f"{playerName} vous avez gagner car le {player1} bat le {player2}")
-elif player1 == "pierre" and player2 == "papier":
-    print(f"je t'ai battu {playerName}car le {player2} bat le {player1}")
+def main():
+    game()
+    print(victory())
+    #replay(playerName);
 
 
+##main();
+print(game())
