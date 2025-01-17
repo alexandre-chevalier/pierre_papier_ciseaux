3cc78567b176813aabbca0b0956d9f29342f3eec
alexandre <alexandre.chevalier@laplateforme.io>
2025-01-17 11:44:55 +0100

    nouvelle version du code

--- ppc.py
+++ ppc.py
@@ -1,79 +1,65 @@
 import random
 
-def game():
-
-    symbol = ["pierre","papier","ciseaux"];
-
+def gamer(symbol):
     print(f"veuillez choisir un symbole")
-
     for i in range(len(symbol)):
         print(f"symbole {i} : " + symbol[i]);
 
     player = symbol[int(input("veuillez choisir un chiffre entre 0 et 2 pour choisir votre symbole : "))]
     bot = symbol[random.randint(0,2)]
-
     print(f"vous avez choisie : {player}")
     print(f"le bot a choisie : {bot}")
-    listsymb = [player, bot];
-    print(listsymb)
-    return victory(listsymb);
 
-def victory(gameplay):
-    print("supersonic")
-    victoryGamePlayer = 0;
-    victoryGameBot = 0;
+    return player, bot
 
-    if victoryGameBot == 3:
-        return print("le bot a gagner")
-        replay();
-    elif victoryGamePlayer == 3:
-        return print(f"vous avez gagner {playerName}")
-        replay(playerName);
-    else:
-            if gameplay[0] == gameplay[1]:
-                print("ceci est une manche nulle")
-                game();
-            elif gameplay[0] == "ciseaux" and gameplay[1] == "papier":
-                print(f"vous avez gagner car le {gameplay[0]} bat le {gameplay[1]}")
-                victoryGamePlayer +=1;
-                game();
-            elif gameplay[0] == "papier" and gameplay[1] == "ciseaux":
-                print(f"je t'ai battu car le {gameplay[1]} bat le {gameplay[0]}")
-                victoryGameBot +=1
-                game();
-            elif gameplay[0] == "pierre" and gameplay[1] == "ciseaux":
-                print(f" vous avez gagner car le {gameplay[0]} bat le {gameplay[1]}")
-                victoryGamePlayer +=1;
-                game();
-            elif gameplay[0] == "ciseaux" and gameplay[1] == "pierre":
-                print(f"je t'ai battu car le {gameplay[1]} bat le {gameplay[0]}")
-                victoryGameBot +=1
-                game();
-            elif gameplay[0] == "papier" and gameplay[1] == "pierre":
-                print(f"vous avez gagner car le {gameplay[0]} bat le {gameplay[1]}")
-                victoryGamePlayer +=1;
-                game();
-            elif gameplay[0] == "pierre" and gameplay[1] == "papier":
-                print(f"je t'ai battu car le {gameplay[1]} bat le {gameplay[0]}")
-                victoryGameBot +=1
-                game();
-        
-       
-def replay(playername):
-    choice = input("Voulez vous rejouer ? (y/n)").upper();
-    if choice == "Y":
-        game();
-        victory(playerName, game(playerName));
-    else:
-        return print(f"merci d'avoir jouer {playername} ;)")
+
+def victory_one_game(gameplay, partie_player, partie_bot):
+    player= gameplay[0]
+    bot= gameplay[1]
+
+    if player == "ciseaux" and bot == "papier":
+        print("player 1 vous avez gagner une manche")
+        partie_player.append("V")
+        return partie_player
+    elif player == "papier" and bot == "ciseaux":
+        print("le bot a gagner la manche")
+        partie_bot.append("V")
+        return partie_bot 
+    elif player == "papier" and bot == "pierre":
+        print("player 1 vous avez gagner une manche")
+        partie_player.append("V")
+        return partie_player
+    elif player == "pierre" and bot == "papier":
+        print("le bot a gagner la manche")
+        partie_bot.append("V")
+        return partie_bot 
+    elif player == "ciseaux" and bot == "pierre":
+        print("player 1 vous avez gagner une manche")
+        partie_player.append("V")
+        return partie_player
+    elif player == "pierre" and bot == "ciseaux":
+        print("le bot a gagner la manche")
+        partie_bot.append("V")
+        return partie_bot 
+
     
 
-
+def result():
+    print("votre resultat")
+    
 def main():
-    game()
-    print(victory())
-    #replay(playerName);
+    manche_player = []
+    manche_bot = []
+    while True:
+        symbol = ["pierre","papier","ciseaux"];
+        gamers = gamer(symbol)
+        victory_one_game(gamers, manche_player, manche_bot)
+
+        print(f"vous avez gagner {manche_player.count("V")} manche ")
+        print(f"le bot a gagner {manche_bot.count("V")} manche ")
 
 
-##main();
-print(game())
+        print(gamers)
+
+
+main()
